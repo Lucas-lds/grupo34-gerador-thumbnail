8f140f8438b2809a04a0bc379fbfa1e6d8ec0375
jeang <jeangabriel180@hotmail.com>
2025-03-15 11:38:28 -0300

    initial commit

---
+++ src/main/java/com/geradorthumbnail/geradorThumbnail/GeradorThumbnailApplication.java
@@ -0,0 +1,13 @@
+package com.geradorthumbnail.geradorThumbnail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeradorThumbnailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeradorThumbnailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/geradorthumbnail/geradorThumbnail/GeradorThumbnailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geradorthumbnail.geradorThumbnail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeradorThumbnailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
