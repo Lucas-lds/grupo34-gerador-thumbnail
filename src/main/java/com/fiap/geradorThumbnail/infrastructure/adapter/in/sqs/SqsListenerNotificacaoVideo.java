4c8a32c07fe62020e77e3eb51aba38c589b1d820
jeang <jeangabriel180@hotmail.com>
2025-04-02 20:23:36 -0300

    adicionando logica para envio de video s3 + notificacao

--- src/main/java/com/fiap/geradorThumbnail/infrastructure/adapter/in/sqs/SqsListenerNotificacaoVideo.java
+++ src/main/java/com/fiap/geradorThumbnail/infrastructure/adapter/in/sqs/SqsListenerNotificacaoVideo.java
@@ -4,12 +4,12 @@ import io.awspring.cloud.sqs.annotation.SqsListener;
 import org.springframework.stereotype.Component;
 
 @Component
-public class SqsListenerProcessamento {
+public class SqsListenerNotificacaoVideo {
 
     @SqsListener("sqs-solicitacao-processamento")
     public void listen(String messageBody) {
         System.out.println("📥 Mensagem recebida com sucesso: " + messageBody);
 
-        // TODO: adicionar lógica posteriormente
+        // TODO: adicionar lógica posteriormente para processamento
     }
 }
