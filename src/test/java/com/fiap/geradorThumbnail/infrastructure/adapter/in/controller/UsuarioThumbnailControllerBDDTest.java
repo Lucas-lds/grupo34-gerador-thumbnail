052c6c729ab1830a4534b3dbbfe42b468f3ce607
Lucas Lindo <lucaslindo711@hotmail.com>
2025-04-18 08:42:26 -0300

    incluindo testes e sns

--- src/test/java/com/fiap/geradorThumbnail/infrastructure/adapter/in/controller/UsuarioThumbnailControllerBDDTest.java
+++ src/test/java/com/fiap/geradorThumbnail/infrastructure/adapter/in/controller/UsuarioThumbnailControllerBDDTest.java
@@ -181,7 +181,7 @@ public class UsuarioThumbnailControllerBDDTest {
 
             ArgumentCaptor<java.util.List> captor = ArgumentCaptor.forClass(java.util.List.class);
             verify(salvarVideoUseCase).executar(captor.capture());
-            verify(snsService).sendNotification("Processamento do vídeo finalizado", "Thumbnail Generator");
+verify(snsService).sendNotification("Processamento do vídeo finalizado.", "Thumbnail Generator");
 
             List capturedList = captor.getValue();
             assertThat(capturedList).isNotEmpty();
