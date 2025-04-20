fe6f3d80dd6b6da702b24148c992580f62ff192a
Lucas Lindo <lucaslindo711@hotmail.com>
2025-04-20 20:47:13 -0300

    ajuste lambda

--- terraform/lambda-create-user.tf
+++ terraform/lambda-create-user.tf
@@ -1,25 +1,11 @@
-resource "null_resource" "prepare_lambda_code" {
-  provisioner "local-exec" {
-    command = <<-EOT
-      mkdir -p ./lambda_package
-      pip install boto3 -t ./lambda_package
-      cp ../lambda-create-user/lambda_function.py ./lambda_package/  # ✅ ALTERADO: caminho relativo
-      cd ./lambda_package && zip -r ../lambda-create-user/lambda_function.zip .  # ✅ ALTERADO: caminho relativo
-    EOT
-  }
-
-  triggers = {
-    code_hash = filesha256("../lambda-create-user/lambda_function.zip")  # ✅ ALTERADO: caminho relativo
-  }
-}
-
 resource "aws_lambda_function" "create_user_function" {
   function_name = "CreateUserFunction"
   role          = aws_iam_role.lambda_exec_role.arn
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.9"
-  filename      = "../lambda-create-user/lambda_function.zip"  # ✅ ALTERADO: caminho relativo
-  source_code_hash = filebase64sha256("../lambda-create-user/lambda_function.zip")  # ✅ ALTERADO: caminho relativo
+
+  filename         = "../lambda-create-user/lambda_function.zip"
+  source_code_hash = filebase64sha256("../lambda-create-user/lambda_function.zip")
 
   environment {
     variables = {
@@ -30,8 +16,6 @@ resource "aws_lambda_function" "create_user_function" {
   }
 
   timeout = 10
-
-  depends_on = [null_resource.prepare_lambda_code]
 
   lifecycle {
     create_before_destroy = true
--- terraform/lambda-persist-user.tf
+++ terraform/lambda-persist-user.tf
@@ -1,25 +1,11 @@
-resource "null_resource" "prepare_code_lambda_persist_user" {
-  provisioner "local-exec" {
-    command = <<-EOT
-      mkdir -p ./lambda_package
-      pip install boto3 -t ./lambda_package
-      cp ../lambda-persist-user/lambda_function.py ./lambda_package/  # ✅ ALTERADO: caminho relativo
-      cd ./lambda_package && zip -r ../lambda-persist-user/lambda_function.zip .  # ✅ ALTERADO: caminho relativo
-    EOT
-  }
-
-  triggers = {
-    code_hash = filesha256("../lambda-persist-user/lambda_function.zip")  # ✅ ALTERADO: caminho relativo
-  }
-}
-
 resource "aws_lambda_function" "persist_user_lambda" {
-  filename         = "../lambda-persist-user/lambda_function.zip"  # ✅ ALTERADO: caminho relativo
-  function_name    = "PersistUserFunction"
-  role             = aws_iam_role.lambda_exec_role.arn
-  handler          = "lambda_function.lambda_handler"
-  runtime          = "python3.9"
-  source_code_hash = filebase64sha256("../lambda-persist-user/lambda_function.zip")  # ✅ ALTERADO: caminho relativo
+  function_name = "PersistUserFunction"
+  role          = aws_iam_role.lambda_exec_role.arn
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.9"
+
+  filename         = "../lambda-persist-user/lambda_function.zip"
+  source_code_hash = filebase64sha256("../lambda-persist-user/lambda_function.zip")
 
   environment {
     variables = {
@@ -29,8 +15,6 @@ resource "aws_lambda_function" "persist_user_lambda" {
       DB_NAME     = var.mysql_database
     }
   }
-
-  depends_on = [null_resource.prepare_code_lambda_persist_user]
 
   lifecycle {
     create_before_destroy = true
