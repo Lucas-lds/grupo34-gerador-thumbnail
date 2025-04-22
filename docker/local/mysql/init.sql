570351012bf5bb05810bffa8512e6e15c275b9dc
Lucas-lds <49286712+Lucas-lds@users.noreply.github.com>
2025-04-22 17:07:03 -0300

    Update init.sql

--- docker/local/mysql/init.sql
+++ docker/local/mysql/init.sql
@@ -1,16 +1,26 @@
-CREATE TABLE tb_processamentos (
-    id BIGINT NOT NULL AUTO_INCREMENT,
-    id_cliente VARCHAR(64) NOT NULL,
-    nome_video VARCHAR(255) NOT NULL,
-    status_processamento VARCHAR(32) NOT NULL,
-    criado_em DATETIME DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (id)
-);
-
 CREATE TABLE IF NOT EXISTS tb_usuarios (
-    id_usuario INT AUTO_INCREMENT PRIMARY KEY,
+    id_usuario BIGINT AUTO_INCREMENT PRIMARY KEY,
+    cognito_user_id VARCHAR(255) NOT NULL,
     nome VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     senha VARCHAR(255) NOT NULL,
     telefone VARCHAR(255) NOT NULL
 );
+
+-- Inserir dados iniciais na tabela de usuários
+INSERT INTO tb_usuarios (nome, cognito_user_id, email, senha, telefone)
+VALUES
+('usuario_exemplo', '12345678-1234-1234-1234-123456789012', 'usuario_exemplo@example.com', 'senha123', '11999999999');
+
+CREATE TABLE IF NOT EXISTS tb_processamentos (
+    id CHAR(36) PRIMARY KEY,
+    id_usuario BIGINT NOT NULL,
+    nome_video VARCHAR(255) NOT NULL,
+    status_processamento VARCHAR(32) NOT NULL,
+    criado_em DATETIME DEFAULT CURRENT_TIMESTAMP,
+    data_atualizacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    CONSTRAINT fk_tb_processamentos_id_usuario
+        FOREIGN KEY (id_usuario) REFERENCES tb_usuarios(id_usuario)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
